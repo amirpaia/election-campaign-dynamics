464cf4b86bcab3c63b1e0e063ab99f0884b0376b
amirpaia <amirpaia@gmail.com>
2021-12-09 01:26:54 +0100

    developing the website with Flask to be able to use prebious python codes

--- Website/FlaskWebsite/FlaskWebsite/views.py
+++ Website/FlaskWebsite/FlaskWebsite/views.py
@@ -2,8 +2,10 @@
 Routes and views for the flask application.
 """
 
-from datetime import datetime
+from datetime import datetime, timedelta
+from flask import request, redirect
 from flask import render_template
+from flask.wrappers import Response
 from FlaskWebsite import app
 
 @app.route('/')
@@ -12,9 +14,24 @@ def home():
     """Renders the home page."""
     return render_template(
         'index.html',
-        title='Home Page',
-        year=datetime.now().year,
-        amir = 123456
+        title='Election Corpus',
+        DateFrom=datetime.now().date() - timedelta(days=7),
+        DateTo=datetime.now().date(),
+        candidate_list = get_candidate()
+    )
+
+@app.route('/create_corpus', methods=['POST'])
+def create_corpus():
+    TwitterId = request.form['TwitterId']
+
+    # your code
+    return render_template(
+        'index.html',
+        title='Election Corpus',
+        DateFrom=datetime.now().date() - timedelta(days=7),
+        DateTo=datetime.now().date(),
+        candidate_list = get_candidate(),
+        response = 1
     )
 
 @app.route('/contact')
@@ -36,3 +53,22 @@ def about():
         year=datetime.now().year,
         message='Your application description page.'
     )
+
+
+
+def get_candidate():
+    candidate_list = {
+        'n_arthaud': 'Nathalie Arthaud',
+        'upr_asselineau': 'Francois Asselineau',
+        'dupontaignan': 'Nicolas Dupont-Aignan',
+        'Anne_Hidalgo':'Anne Hidalgo',
+        'yjadot':'Yannick Jadot',
+        'jeanlassalle':'Jean Lassalle',
+        'MLP_officiel':'Marine Le Pen',
+        'JLMelenchon':'Jean-Luc Melenchon',
+        'montebourg':'Arnaud Montebourg',
+        'jfpoisson78':'Jean-Frederic Poisson',
+        'PhilippePoutou':'Philippe Poutou',
+        'Fabien_Roussel':'Fabien Roussel'
+    }
+    return candidate_list
